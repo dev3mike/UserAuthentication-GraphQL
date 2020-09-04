a66255559de4e3e35adf303d323f378c7ffd1cc9
max-macbook <masoudx@gmail.com>
2020-09-04 15:07:07 +0430

    First Commit

---
+++ src/entity/User.ts
@@ -0,0 +1,31 @@
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
+import { Field, ID, ObjectType } from "type-graphql";
+
+@ObjectType()
+@Entity()
+export class User extends BaseEntity {
+
+    @Field(() => ID)
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    // Custom GraphQL Field
+    @Field()
+    name: string;
+
+    @Field()
+    @Column()
+    firstName: string;
+
+    @Field()
+    @Column()
+    lastName: string;
+
+    @Field()
+    @Column("text", { unique: true })
+    email: string;
+
+    @Column()
+    password: string;
+
+}
